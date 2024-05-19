631831c474b0b38ead1ea2e86f051b51ee27c629
VinylmastersLtd <140657595+VinylmastersLtd@users.noreply.github.com>
2024-05-19 16:15:42 +0100

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -61,7 +61,7 @@ export function NavBar() {
                                 </Link>
                             </DialogClose>
                             <DialogClose asChild>
-                                <Link href={userId ? "/dashboard" : "/sign-in"} legacyBehavior passHref className="cursor-pointer">
+                                <Link href="/dashboard" legacyBehavior passHref className="cursor-pointer">
                                     <Button variant="outline">
                                         Dashboard
                                     </Button>
@@ -72,81 +72,6 @@ export function NavBar() {
                 </Dialog>
                 <ModeToggle />
             </div>
-            <NavigationMenu>
-                <NavigationMenuList className="max-[825px]:hidden flex gap-3 w-[100%] justify-between">
-                    <Link href="/" className="pl-2">
-                        <BlocksIcon />
-                    </Link>
-                </NavigationMenuList>
-                <NavigationMenuList>
-                    <NavigationMenuItem className="max-[825px]:hidden ml-5">
-                        <NavigationMenuTrigger>Features</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <ul className="flex flex-col w-[400px] gap-3 p-4 lg:w-[500px]">
-                                {components.map((component) => (
-                                    <ListItem
-                                        key={component.title}
-                                        title={component.title}
-                                        href={component.href}
-                                    >
-                                        {component.description}
-                                    </ListItem>
-                                ))}
-                            </ul>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                    <NavigationMenuList>
-                        <Link className="max-[825px]:hidden" href="https://learn.rankboost.ai" target="_blank">
-                            <Button variant="ghost">Docs</Button>
-                        </Link>
-                    </NavigationMenuList>
-                </NavigationMenuList>
-            </NavigationMenu>
-            <div className="flex items-center gap-3 max-[825px]:hidden">
-                <Link href={userId ? "/dashboard" : "/sign-in"}>
-                    <Button size="sm">Dashboard</Button>
-                </Link>
-                <SignedOut>
-                    <Link href="/sign-in">
-                        <Button size="sm" variant="ghost">Sign In</Button>
-                    </Link>
-                    <Link href="/sign-up">
-                        <Button size="sm" variant="ghost">Sign Up</Button>
-                    </Link>
-                </SignedOut>
-                <SignedIn>
-                    <Profile />
-                </SignedIn>
-                <ModeToggle />
-            </div>
-        </div>
-    );
-}
-
-const ListItem = React.forwardRef(({
-    className, title, children, ...props
-}, ref) => {
-    return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    );
-});
-ListItem.displayName = "ListItem";
             <NavigationMenu>
                 <NavigationMenuList className="max-[825px]:hidden flex gap-3 w-[100%] justify-between">
                     <Link href="/" className="pl-2">
