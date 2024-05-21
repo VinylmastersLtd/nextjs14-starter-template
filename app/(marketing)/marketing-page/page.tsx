a8fe0e418c288f97e1fa0d3f8e17d922970e1b1e
VinylmastersLtd <140657595+VinylmastersLtd@users.noreply.github.com>
2024-05-21 17:52:53 +0100

    Update page.tsx

--- app/(marketing)/marketing-page/page.tsx
+++ app/(marketing)/marketing-page/page.tsx
@@ -1,33 +1,30 @@
-"use client";
+import PageWrapper from '@/components/Container/PageWrapper'
+import { Button } from '@/components/ui/button'
+import { Metadata } from 'next'
+import Link from 'next/link'
+import React from 'react'
 
-import PageWrapper from '@/components/Container/PageWrapper';
-import { Button } from '@/components/ui/button';
-import CreateSearchForm from '@/components/ui/CreateSearchForm';
-import GenerateSerialNumbers from '@/components/ui/GenerateSerialNumbers';
-import SearchForm from '@/components/ui/SearchForm';
-import ManagePage from '@/components/ui/ManagePage';
-import Link from 'next/link';
-import React, { useState } from 'react';
-
-interface MetaField {
-  name: string;
-  value: string;
+export const metadata: Metadata = {
+  metadataBase: new URL("https://starter.rasmic.xyz"),
+  keywords: [''],
+  title: 'Marketing page',
+  openGraph: {
+    description: 'Put description of the page.',
+    images: ['']
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Marketing page',
+    description: 'Put description of the page.',
+    siteId: "",
+    creator: "@rasmickyy",
+    creatorId: "",
+    images: [''],
+  },
 }
 
-interface SerialNumber {
-  id: string;
-  metaFields: MetaField[];
-  createdAt: Date;
-  expiryDate: Date;
-}
 
-export default function MarketingPage() {
-  const [serialNumbers, setSerialNumbers] = useState<SerialNumber[]>([]);
-  const [formId] = useState(Date.now().toString());
-
-  const addSerialNumber = (serialNumber: SerialNumber) => {
-    setSerialNumbers([...serialNumbers, serialNumber]);
-  };
+export default async function MarketingPage() {
 
   return (
     <PageWrapper>
@@ -46,13 +43,74 @@ export default function MarketingPage() {
             <source src="https://www.youtube.com/watch?v=ml9Fz2aUx9k" type="video/mp4" />
           </video>
         </div>
-        <div className='flex flex-col max-w-[900px] items-center my-[2rem]'>
-          <CreateSearchForm />
-          <GenerateSerialNumbers addSerialNumber={addSerialNumber} />
-          <SearchForm serialNumbers={serialNumbers} />
-          <ManagePage formId={formId} />
+        <div className='flex flex-col min-h-screen max-w-[900px] items-center my-[2rem]'>
+          <article className="max-w-4xl mx-auto pb-8">
+            <h1 className="text-3xl lg:text-4xl font-bold mb-6">Lorem ipsum dolor sit amet</h1>
+
+            <section className="mb-8">
+              <h2 className="mt-10 scroll-m-20 border-b pb-2 mb-3 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Lorem ipsum</h2>
+              <p className="text-md leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </section>
+
+            <section className="mb-8">
+              <h2 className="mt-10 scroll-m-20 border-b pb-2 mb-3 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Lorem ipsum</h2>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </section>
+
+            <section className="mb-8">
+              <h2 className="mt-10 scroll-m-20 border-b pb-2 mb-3 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Lorem ipsum</h2>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <ol className="flex flex-col gap-1 list-decimal ml-8 mb-4">
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              </ol>
+            </section>
+
+            <section className="mb-8">
+              <h2 className="mt-10 scroll-m-20 border-b pb-2 mb-3 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Lorem ipsum</h2>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <ul className=" flex flex-col gap-1 list-disc ml-8 mb-4">
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              </ul>
+            </section>
+
+            <section className="mb-8">
+              <h2 className="mt-10 scroll-m-20 border-b pb-2 mb-3 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Lorem ipsum</h2>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <ul className=" flex flex-col gap-1 list-disc ml-8 mb-4">
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              </ul>
+            </section>
+
+            <section className="mb-8">
+              <h2 className="mt-10 scroll-m-20 border-b pb-2 mb-3 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Lorem ipsum</h2>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <ul className=" flex flex-col gap-1 list-disc ml-8 mb-4">
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                <li className="mb-2"><strong>Lorem ipsum dolor sit amet:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+              </ul>
+            </section>
+
+            <section className="mb-8">
+              <h2 className="mt-10 scroll-m-20 border-b pb-2 mb-3 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Lorem ipsum</h2>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <p className="text-md mb-5 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </section>
+
+            <section>
+              <h2 className="mt-10 scroll-m-20 border-b pb-2 mb-3 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Lorem ipsum</h2>
+              <p className="text-md   leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </section>
+          </article>
         </div>
+
       </div>
     </PageWrapper>
-  );
+  )
 }
