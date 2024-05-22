ffc47d684ea5833f724b94612b6d87add5183ab4
VinylmastersLtd <vinylmastersltd@gmail.com>
2024-05-22 21:47:14 +0100

    kk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import PageWrapper from "@/components/Container/PageWrapper";
 import { AccordionComponent } from "@/components/LandingPage/AccordionComponent";
 import BlogSample from "@/components/LandingPage/BlogSamples";
@@ -7,10 +5,8 @@ import Footer from "@/components/LandingPage/Footer";
 import HeroSection from "@/components/LandingPage/HeroSection";
 import MarketingCards from "@/components/LandingPage/MarketingCards";
 import PricingPage from "@/components/LandingPage/Pricing";
-import { useScroll, useTransform } from "framer-motion";
-import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+import { GoogleGeminiEffect } from "@/components/LandingPage/google-gemini-effect";
 
-import React from "react";
 export default function Home() {
   return (
     <PageWrapper>
@@ -30,41 +26,12 @@ export default function Home() {
       <div className="flex justify-center items-center w-full mt-[5rem] mb-[9rem]">
         <AccordionComponent />
       </div>
+      <div className="flex justify-center items-center w-full mt-[5rem] mb-[9rem]">
+        <GoogleGeminiEffect pathLengths={[]} />
+      </div>
       {/* <div className="w-full">
         <Footer />
       </div> */}
     </PageWrapper>
-  );
-}
-
-
-export function GoogleGeminiEffectDemo() {
-  const ref = React.useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
-  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
-  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
-  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
-  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
-
-  return (
-    <div
-      className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] rounded-md relative pt-40 overflow-clip"
-      ref={ref}
-    >
-      <GoogleGeminiEffect
-        pathLengths={[
-          pathLengthFirst,
-          pathLengthSecond,
-          pathLengthThird,
-          pathLengthFourth,
-          pathLengthFifth,
-        ]}
-      />
-    </div>
   );
 }
